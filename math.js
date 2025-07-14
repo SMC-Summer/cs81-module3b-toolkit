5f3f286628300427cb9bc833ff17443309c1b22a
SMC-Summer <killebrew_dante_st01@student.smc.edu>
2025-07-13 19:07:02 -0700

    Create math.js and Add Double Function

---
+++ math.js
@@ -0,0 +1,14 @@
+// A simple function that doubles a number.
+function double(n) {
+  return n * 2;
+}
+
+// A function that squares a number.
+function square(n) {
+  return n * n;
+}
+
+// A function that multiplies two numbers.
+function multiply(a, b) {
+  return a * b;
+}
